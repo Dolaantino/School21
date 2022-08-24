8323db83ce03996de3cd13f801c22fb80433ec8c
Dolaantino <89834351+Dolaantino@users.noreply.github.com>
2022-08-24 19:19:28 +0300

    Add files via upload

---
+++ T07D10-0/src/pointer.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (){
+    scanf("%d%d%d", &a, &b, &c);
+    if (a > b , c)
+    printf ("maximum = %d\n", a);
+    else if (b > a , c)    
+    printf ("maximum = %d\n",b);
+    return 0;
+}
