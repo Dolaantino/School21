37dfa3c13e87e2c7e34b94684f68fae0363f7445
Dolaantino <89834351+Dolaantino@users.noreply.github.com>
2023-05-10 14:59:28 +0300

    Delete T08D11-0 directory

--- T08D11-0/src/det.c
+++
@@ -1,9 +0,0 @@
-double det(double **matrix, int n, int m);
-void input(double **matrix, int *n, int *m);
-void output(double det);
-
-void main()
-{
-
-}
-
--- T08D11-0/src/invert.c
+++
@@ -1,9 +0,0 @@
-void invert(double **matrix, int n, int m);
-void input(double **matrix, int *n, int *m);
-void output(double **matrix, int n, int m);
-
-
-void main()
-{
-
-}
