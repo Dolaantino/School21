f3fcaeae9c089d55aeb50e47dd7a967fdec636cc
Dolaantino <89834351+Dolaantino@users.noreply.github.com>
2023-05-10 14:58:38 +0300

    Delete T03D03-0 directory

--- T03D03-0/src/bfbf.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main () 
-{
-    int a=5;
-    int *px =&a;
-    cout << px << endl;
-    return 0;
-}
--- T03D03-0/src/named_hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main (void) {
-    
-    int name;
-    scanf("%d",&name);
-    printf("Hello, %d!",name);
-    return 0;
-}
